ca4e91a6f42c49e81e63c181d521550418537f3e
Jesse Szwedko <j.szwedko@modcloth.com>
2013-08-12 12:13:34 -0400

    Injecting socket to logger
    Signed-off-by: Sheena McCoy <sp.mccoy@modcloth.com>

--- lib/logstash4r/logger.rb
+++ lib/logstash4r/logger.rb
@@ -1,17 +1,12 @@
-require 'json'
-require 'socket'
 require 'logger'
-require 'logstash4r/socket'
 require 'logstash4r/formatters/plain'
-require 'logstash4r/formatters/json'
-require 'logstash4r/formatters/json_event'
 
 module LogStash4r
   class Logger < ::Logger
-    def initialize(host, port, socket_type = :udp, formatter = LogStash4r::Formatters::Plain)
+    def initialize(socket, formatter = LogStash4r::Formatters::Plain)
       extend formatter
 
-      super(LogStash4r::Socket.new(host, port, socket_type))
+      super(socket)
     end
   end
 end
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -1,63 +1,59 @@
 require 'logstash4r'
+require 'logstash4r/formatters/json'
+require 'logstash4r/formatters/json_event'
+require 'logstash4r/formatters/plain'
+require 'json'
+require 'socket'
 
 describe LogStash4r::Logger do
-  subject { LogStash4r::Logger.new(host, port) }
+  subject { LogStash4r::Logger.new(socket) }
 
-  let(:host) { 'localhost' }
-  let(:port) { '80' }
   let(:socket) { double('socket').as_null_object }
   let(:now) { Time.now }
 
   before do
-    ::Socket.stub(:new).and_return(socket)
     socket.stub(:connect)
     Time.stub(now: now)
   end
 
   context 'plain format' do
-    %w(udp tcp).each do |socket_type|
-      subject { LogStash4r::Logger.new(host, port, socket_type.to_sym, LogStash4r::Formatters::Plain) }
+    subject { LogStash4r::Logger.new(socket, LogStash4r::Formatters::Plain) }
 
-      %w(debug info warn error fatal).each do |severity|
-        it "writes a plain #{severity} message to the socket #{socket_type}" do
-          socket.should_receive(:write).with(/#{severity.upcase} test/)
-          subject.send(severity.to_sym, 'test')
-        end
+    %w(debug info warn error fatal).each do |severity|
+      it "writes a plain #{severity} message to the socket" do
+        socket.should_receive(:write).with(/#{severity.upcase} test/)
+        subject.send(severity.to_sym, 'test')
+      end
+    end
+  end
+
+  context 'json format' do
+    subject { LogStash4r::Logger.new(socket, LogStash4r::Formatters::Json) }
+
+    %w(debug info warn error fatal).each do |severity|
+      it "writes a json #{severity} message to the socket" do
+        socket.should_receive(:write).with({
+          message: {data: 'test'},
+          severity: severity.upcase,
+          timestamp: now,
+        }.to_json)
+        subject.send(severity.to_sym, {data: 'test'})
       end
     end
 
-    context 'json format' do
-      %w(udp tcp).each do |socket_type|
-        subject { LogStash4r::Logger.new(host, port, socket_type.to_sym, LogStash4r::Formatters::Json) }
+    context 'json event format' do
+      subject { LogStash4r::Logger.new(socket, LogStash4r::Formatters::JsonEvent) }
 
-        %w(debug info warn error fatal).each do |severity|
-          it "writes a json #{severity} message to the socket #{socket_type}" do
-            socket.should_receive(:write).with({
-              message: {data: 'test'},
-              severity: severity.upcase,
-              timestamp: now,
-            }.to_json)
-            subject.send(severity.to_sym, {data: 'test'})
-          end
-        end
-      end
-
-      context 'json event format' do
-        %w(udp tcp).each do |socket_type|
-          subject { LogStash4r::Logger.new(host, port, socket_type.to_sym, LogStash4r::Formatters::JsonEvent) }
-
-          %w(debug info warn error fatal).each do |severity|
-            it "writes a json #{severity} message to the socket #{socket_type}" do
-              socket.should_receive(:write).with({
-                :@source => ::Socket::gethostname,
-                :@timestamp => now,
-                :@tags => [],
-                :@fields => {severity: severity.upcase},
-                :@message => 'test'
-              }.to_json + "\n")
-              subject.send(severity.to_sym, {:@message => 'test'})
-            end
-          end
+      %w(debug info warn error fatal).each do |severity|
+        it "writes a json #{severity} message to the socket" do
+          socket.should_receive(:write).with({
+            :@source => ::Socket::gethostname,
+            :@timestamp => now,
+            :@tags => [],
+            :@fields => {severity: severity.upcase},
+            :@message => 'test'
+          }.to_json + "\n")
+          subject.send(severity.to_sym, {:@message => 'test'})
         end
       end
     end
